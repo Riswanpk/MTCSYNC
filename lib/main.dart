1421708e88e05c499eed8f428caa99f9f19d6d51
Your Name <your_email@example.com>
2025-05-18 14:01:05 +0530

    main updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'login.dart'; // Make sure login.dart is in the same folder
+import 'constant.dart';
+import 'login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // 👈 Initialize Firebase before runApp
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: firebaseApiKey,
+      appId: firebaseAppId,
+      messagingSenderId: firebaseMessagingSenderId,
+      projectId: firebaseProjectId,
+      authDomain: firebaseAuthDomain,
+      storageBucket: firebaseStorageBucket,
+      measurementId: firebaseMeasurementId,
+    ),
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
