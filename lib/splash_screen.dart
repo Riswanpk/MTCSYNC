e65671bea6c270dc15940dad845822310855ce20
Your Name <your_email@example.com>
2025-05-21 19:07:24 +0530

    splash screen fix

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,23 +1,45 @@
-// splash_screen.dart
 import 'package:flutter/material.dart';
-import 'home.dart'; // Import your home.dart file
+import 'package:firebase_auth/firebase_auth.dart';
+import 'home.dart';
+import 'login.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const HomePage()), // Navigate to HomePage
-      );
-    });
+  State<SplashScreen> createState() => _SplashScreenState();
+}
 
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      final user = FirebaseAuth.instance.currentUser;
+
+      if (user == null) {
+        // User not logged in
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginPage()),
+        );
+      } else {
+        // User logged in
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const HomePage()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Image.asset(
-          'assets/images/logo.png', // Ensure the path matches your assets folder
+          'assets/images/logo.png',
           width: 200,
         ),
       ),
