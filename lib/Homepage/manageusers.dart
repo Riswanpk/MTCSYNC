1c2ccce9d960ca2f006add784b1bf99ce88d537a
CRM <crmmalabar@gmail.com>
2026-08-25 11:27:33 +0400

    s

--- lib/Homepage/manageusers.dart
+++ lib/Homepage/manageusers.dart
@@ -266,7 +266,7 @@ class _ManageUsersPageState extends State<ManageUsersPage> {
                   ..sort((a, b) => a.key.compareTo(b.key));
 
                 // Get latest version from app_constants.dart
-                const String latestVersion = '2.0.178';
+                const String latestVersion = '2.0.179';
 
                 return AlertDialog(
                   title: Row(
--- lib/Version/app_constants.dart
+++ lib/Version/app_constants.dart
@@ -1,1 +1,1 @@
-const String appVersion = '2.0.178';
+const String appVersion = '2.0.179';
